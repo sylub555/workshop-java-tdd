3f32de626c7a7f74cec9846ece7827f9549f00d8
Anuchit.i <sylub555@gmail.com>
2021-09-30 16:44:00 +0700

    example spy test (Test Double)

--- day01/src/test/java/day01/IdGeneratreServiceTest.java
+++ day01/src/test/java/day01/IdGeneratreServiceTest.java
@@ -9,12 +9,21 @@ import org.junit.jupiter.api.Test;
 class IdGeneratreServiceTest {
 
 	@Test
-	void test() {
+	void test_getData() {
 		Random stub = new Random6();
 		IdGeneratreService service = new IdGeneratreService();
 		service.setRandom(stub);
 		String result = service.getData();
 		assertEquals("CODE6", result);
+	}
+
+	@Test
+	void test_getData2() {
+		SpyRandom spy = new SpyRandom();
+		IdGeneratreService service = new IdGeneratreService();
+		service.setRandom(spy);
+		service.getData();
+		spy.verify(1);
 	}
 
 }
@@ -24,6 +33,22 @@ class Random6 extends Random {
 	@Override
 	public int nextInt(int bound) {
 		return 6;
+	}
+
+}
+
+class SpyRandom extends Random {
+
+	private int count = 0;
+
+	@Override
+	public int nextInt(int bound) {
+		count++;
+		return 6;
+	}
+
+	public void verify(int expected) {
+		assertEquals(expected, count);
 	}
 
 }
