206a6c2ee67ad58e478f4c1d5db75fc4d522db89
Anuchit.i <sylub555@gmail.com>
2021-09-30 16:36:45 +0700

    example stub test (Test Double)

--- day01/src/test/java/day01/IdGeneratreServiceTest.java
+++ day01/src/test/java/day01/IdGeneratreServiceTest.java
@@ -2,15 +2,28 @@ package day01;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Random;
+
 import org.junit.jupiter.api.Test;
 
 class IdGeneratreServiceTest {
 
 	@Test
 	void test() {
+		Random stub = new Random6();
 		IdGeneratreService service = new IdGeneratreService();
+		service.setRandom(stub);
 		String result = service.getData();
 		assertEquals("CODE6", result);
+	}
+
+}
+
+class Random6 extends Random {
+
+	@Override
+	public int nextInt(int bound) {
+		return 6;
 	}
 
 }
