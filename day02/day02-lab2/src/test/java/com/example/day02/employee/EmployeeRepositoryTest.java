a2362c60db481b1157f0e6df2540038acdce1b08
Anuchit.i <sylub555@gmail.com>
2021-10-01 16:40:43 +0700

    Add repository test

---
+++ day02/day02-lab2/src/test/java/com/example/day02/employee/EmployeeRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.example.day02.employee;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class EmployeeRepositoryTest {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Test
+	void case01() {
+		// Arrage
+		Employee demo = new Employee(1, "hello world");
+		employeeRepository.save(demo);
+
+		// Act
+		var result = employeeRepository.findById(1);
+
+		// Assert
+		assertTrue(result.isPresent());
+		assertEquals(1, result.get().getId());
+		assertEquals("hello world", result.get().getName());
+	}
+
+}
