53498f7b93980e2661ea192a083e0961b86b56fa
Anuchit.i <sylub555@gmail.com>
2021-10-01 13:45:15 +0700

    Add log of bean definitions

--- day02/day02-lab2/src/main/java/com/example/day02/Day02Application.java
+++ day02/day02-lab2/src/main/java/com/example/day02/Day02Application.java
@@ -1,13 +1,20 @@
 package com.example.day02;
 
+import java.util.stream.Stream;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class Day02Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Day02Application.class, args);
+		var context = SpringApplication.run(Day02Application.class, args);
+		log.info("bean definition count {}", context.getBeanDefinitionCount());
+		Stream.of(context.getBeanDefinitionNames()).forEach(name -> log.info("bean deinition name {}", name));
 	}
 
 }
