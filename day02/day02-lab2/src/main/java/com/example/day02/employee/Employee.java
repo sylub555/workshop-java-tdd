d918dfa7b68558c31caa56468ec0a8db3a53b376
Anuchit.i <sylub555@gmail.com>
2021-10-01 16:26:12 +0700

    Add repository of employee

---
+++ day02/day02-lab2/src/main/java/com/example/day02/employee/Employee.java
@@ -0,0 +1,25 @@
+package com.example.day02.employee;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "EMPLOYEE")
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ID")
+	private Integer id;
+
+	@Column(name = "NAME")
+	private String name;
+
+}
