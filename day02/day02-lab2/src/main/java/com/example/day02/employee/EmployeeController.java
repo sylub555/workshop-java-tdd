161945e9cbfdb9bf455888cccfa8e6cae7ecdd9c
Anuchit.i <sylub555@gmail.com>
2021-10-01 14:55:46 +0700

    Add exaple employee get all

--- day02/day02-lab2/src/main/java/com/example/day02/employee/EmployeeController.java
+++ day02/day02-lab2/src/main/java/com/example/day02/employee/EmployeeController.java
@@ -1,5 +1,8 @@
 package com.example.day02.employee;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +13,11 @@ public class EmployeeController {
 	@GetMapping("employee/{id}")
 	public EmployeeResponse getById(@PathVariable int id) {
 		return new EmployeeResponse(id, "hello world");
+	}
+
+	@GetMapping("employee")
+	public List<EmployeeResponse> getAll() {
+		return Arrays.asList(new EmployeeResponse(1, "hello"), new EmployeeResponse(2, "world"));
 	}
 
 }
--- day02/day02-lab2/src/test/java/com/example/day02/employee/EmployeeControllerTest.java
+++ day02/day02-lab2/src/test/java/com/example/day02/employee/EmployeeControllerTest.java
@@ -18,10 +18,25 @@ class EmployeeControllerTest {
 	void getById() {
 		// Act
 		var result = restTemplate.getForObject("/employee/1", EmployeeResponse.class);
-		
+
 		// Assert
 		assertEquals(1, result.getId());
 		assertEquals("hello world", result.getName());
+	}
+
+	@Test
+	void getAll() {
+		// Act
+		var results = restTemplate.getForObject("/employee", EmployeeResponse[].class);
+
+		// Assert
+		assertEquals(2, results.length);
+		
+		assertEquals(1, results[0].getId());
+		assertEquals("hello", results[0].getName());
+
+		assertEquals(2, results[1].getId());
+		assertEquals("world", results[1].getName());
 	}
 
 }
