d1ebee94abc8ee61374984dbebaebc14bbd6b881
Anuchit.i <sylub555@gmail.com>
2021-10-01 16:52:15 +0700

    Implement repository to servcie

--- day02/day02-lab2/src/main/java/com/example/day02/employee/EmployeeService.java
+++ day02/day02-lab2/src/main/java/com/example/day02/employee/EmployeeService.java
@@ -1,19 +1,23 @@
 package com.example.day02.employee;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
 
+	private EmployeeRepository repository;
+
 	public EmployeeResponse getId(int id) {
-		return new EmployeeResponse(id, "hello world");
+		return repository.findById(id).map(emp -> new EmployeeResponse(emp.getId(), emp.getName()))
+				.orElseThrow(() -> new RuntimeException("Emplyee not found with id = " + id));
 	}
 
 	public List<EmployeeResponse> getAll() {
-		return Arrays.asList(new EmployeeResponse(1, "hello"), new EmployeeResponse(2, "world"));
+		return repository.findAll().stream().map(emp -> new EmployeeResponse(emp.getId(), emp.getName()))
+				.collect(Collectors.toList());
 	}
 
 }
