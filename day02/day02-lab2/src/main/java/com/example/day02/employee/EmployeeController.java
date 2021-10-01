ea90ee5163cb504558c7507f136271225ff80e0b
Anuchit.i <sylub555@gmail.com>
2021-10-01 14:13:53 +0700

    Add employee controller

---
+++ day02/day02-lab2/src/main/java/com/example/day02/employee/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.example.day02.employee;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+	@GetMapping("employee/{id}")
+	public EmployeeResponse getById(@PathVariable int id) {
+		return new EmployeeResponse(id, "hello world");
+	}
+
+}
---
+++ day02/day02-lab2/src/main/java/com/example/day02/employee/EmployeeResponse.java
@@ -0,0 +1,15 @@
+package com.example.day02.employee;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class EmployeeResponse {
+
+	private Integer id;
+	private String name;
+
+}
