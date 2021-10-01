e75c7be073aeec9a57f294e06a8cbb5892ca880b
Anuchit.i <sylub555@gmail.com>
2021-10-01 11:32:16 +0700

    Refector move calculator free to speaker

--- day02/workshop-java-badcode/src/main/java/badcode/RegisterBusiness.java
+++ day02/workshop-java-badcode/src/main/java/badcode/RegisterBusiness.java
@@ -6,14 +6,10 @@ public class RegisterBusiness {
 
 	public Integer register(SpeakerRepository repository, Speaker speaker) {
 		Integer speakerId;
-		
+
 		// Validate input
 		validateInput(speaker);
-		
-		// Fee calculation
-		int exp = speaker.getExp();
-		speaker.setRegistrationFee(getFee(exp));
-		
+
 		// Save speaker
 		try {
 			speakerId = repository.saveSpeaker(speaker);
@@ -42,20 +38,6 @@ public class RegisterBusiness {
 
 	private boolean isNullOrEmpty(String str) {
 		return str == null || str.trim().equals("");
-	}
-
-	int getFee(int experienceYear) {
-		int fee = 0;
-		if (experienceYear <= 1) {
-			fee = 500;
-		} else if (experienceYear <= 3) {
-			fee = 250;
-		} else if (experienceYear <= 5) {
-			fee = 100;
-		} else if (experienceYear <= 9) {
-			fee = 50;
-		}
-		return fee;
 	}
 
 	public String getEmailDomain(String email) {
--- day02/workshop-java-badcode/src/main/java/badcode/Speaker.java
+++ day02/workshop-java-badcode/src/main/java/badcode/Speaker.java
@@ -3,76 +3,81 @@ package badcode;
 import java.util.List;
 
 public class Speaker {
-    private String firstName;
-    private String lastName;
-    private String email;
-    private int exp;
-    private int registrationFee;
-    private boolean hasBlog;
-    private String blogUrl;
-    private List<String> certifications;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private int exp;
+	private boolean hasBlog;
+	private String blogUrl;
+	private List<String> certifications;
 
-    public String getFirstName() {
-        return firstName;
-    }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public int getExp() {
-        return exp;
-    }
+	public int getExp() {
+		return exp;
+	}
 
-    public void setExp(int exp) {
-        this.exp = exp;
-    }
+	public void setExp(int exp) {
+		this.exp = exp;
+	}
 
-    public int getRegistrationFee() {
-        return registrationFee;
-    }
+	public int getRegistrationFee() {
+		int fee = 0;
+		if (exp <= 1) {
+			fee = 500;
+		} else if (exp <= 3) {
+			fee = 250;
+		} else if (exp <= 5) {
+			fee = 100;
+		} else if (exp <= 9) {
+			fee = 50;
+		}
+		return fee;
+	}
 
-    public void setRegistrationFee(int registrationFee) {
-        this.registrationFee = registrationFee;
-    }
+	public boolean isHasBlog() {
+		return hasBlog;
+	}
 
-    public boolean isHasBlog() {
-        return hasBlog;
-    }
+	public void setHasBlog(boolean hasBlog) {
+		this.hasBlog = hasBlog;
+	}
 
-    public void setHasBlog(boolean hasBlog) {
-        this.hasBlog = hasBlog;
-    }
+	public String getBlogUrl() {
+		return blogUrl;
+	}
 
-    public String getBlogUrl() {
-        return blogUrl;
-    }
+	public void setBlogUrl(String blogUrl) {
+		this.blogUrl = blogUrl;
+	}
 
-    public void setBlogUrl(String blogUrl) {
-        this.blogUrl = blogUrl;
-    }
+	public List<String> getCertifications() {
+		return certifications;
+	}
 
-    public List<String> getCertifications() {
-        return certifications;
-    }
-
-    public void setCertifications(List<String> certifications) {
-        this.certifications = certifications;
-    }
+	public void setCertifications(List<String> certifications) {
+		this.certifications = certifications;
+	}
 }
