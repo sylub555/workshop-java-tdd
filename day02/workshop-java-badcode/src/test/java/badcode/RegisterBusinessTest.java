17a12e4b61ab079984311ee01c808b6c2082c13e
Anuchit.i <sylub555@gmail.com>
2021-10-01 10:09:42 +0700

    Add test case01 check register free

--- day02/workshop-java-badcode/src/test/java/badcode/RegisterBusinessTest.java
+++ day02/workshop-java-badcode/src/test/java/badcode/RegisterBusinessTest.java
@@ -14,12 +14,34 @@ class RegisterBusinessTest {
 		speaker.setLastName("lastName");
 		speaker.setEmail("email@gmail.com");
 		SpeakerRepository repository = new SpeakerRepository() {
+
 			@Override
 			public Integer saveSpeaker(Speaker speaker) {
 				return 100;
 			}
+
 		};
 		Integer speakerId = business.register(repository, speaker);
 		assertEquals(100, speakerId);
+	}
+
+	@Test
+	void case02() {
+		RegisterBusiness business = new RegisterBusiness();
+		Speaker speaker = new Speaker();
+		speaker.setFirstName("firstName");
+		speaker.setLastName("lastName");
+		speaker.setEmail("email@gmail.com");
+		speaker.setExp(0);
+		SpeakerRepository repository = new SpeakerRepository() {
+
+			@Override
+			public Integer saveSpeaker(Speaker speaker) {
+				assertEquals(500, speaker.getRegistrationFee());
+				return 100;
+			}
+
+		};
+		business.register(repository, speaker);
 	}
 }
